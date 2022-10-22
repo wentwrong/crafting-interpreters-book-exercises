2fb29ae43258b719c636094bfb8e9bcb3533659d
Pavel <wabloxa@yandex.ru>
2022-10-22 18:58:23 +0400

    add ugly scanning of some tokens

--- src/main/java/omg/wentwrong/Scanner.java
+++ src/main/java/omg/wentwrong/Scanner.java
@@ -19,16 +19,70 @@ public class Scanner {
         public Token(TokenType tokenType) {
             this(tokenType, null);
         }
+
+        @Override
+        public String toString() {
+            return "Token{" +
+                    "tokenType=" + tokenType +
+                    ", value=" + value +
+                    '}';
+        }
     }
 
     public List<Token> getTokens(String input) {
+        var line = 0;
         var tokens = new ArrayList<Token>();
-        tokens.add(new Token(TokenType.VAR));
-        tokens.add(new Token(TokenType.VAR_NAME, "a"));
-        tokens.add(new Token(TokenType.EQUAL));
-        tokens.add(new Token(TokenType.INT_NUMBER, 1));
-        tokens.add(new Token(TokenType.PLUS));
-        tokens.add(new Token(TokenType.INT_NUMBER, 2));
+        for (var i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            switch (ch) {
+                case ' ':
+                    break;
+                case '=':
+                    tokens.add(new Token(TokenType.EQUAL));
+                    break;
+                case '+':
+                    tokens.add(new Token(TokenType.PLUS));
+                    break;
+                case '\n':
+                    line++;
+                    break;
+                default:
+                    if (Character.isDigit(ch)) {
+                        var j = i + 1;
+                        while (j < input.length()) {
+                            if (!Character.isDigit(input.charAt(j))) {
+                                break;
+                            }
+                            j += 1;
+                        }
+                        var number = Integer.parseInt(input.substring(i, j));
+                        i = j;
+                        tokens.add(new Token(TokenType.INT_NUMBER, number));
+                    } else if (Character.isAlphabetic(ch)) {
+                        var j = i + 1;
+                        while (j < input.length()) {
+                            if (!Character.isAlphabetic(input.charAt(j))) {
+                                break;
+                            }
+                            j += 1;
+                        }
+                        var string = input.substring(i, j);
+                        i = j;
+                        if (string.equals("var")) {
+                            tokens.add(new Token(TokenType.VAR));
+                            continue;
+                        }
+                        tokens.add(new Token(TokenType.VAR_NAME, string));
+                    } else {
+                        reportError(String.format("Found unknown character: '%c'", ch), i, line);
+                    }
+            }
+        }
+        tokens.add(new Token(TokenType.EOF));
         return tokens;
+    }
+
+    private void reportError(String message, int column, int line) {
+        System.out.printf("Error while scanning [%d:%d]:\n%s\n", column, line, message);
     }
 }
--- src/test/groovy/ScannerSpec.groovy
+++ src/test/groovy/ScannerSpec.groovy
@@ -1,16 +1,21 @@
-import spock.lang.Specification
 import omg.wentwrong.Scanner
 import omg.wentwrong.TokenType
+import spock.lang.Specification
 
 class ScannerSpec extends Specification {
     def scanner = new Scanner()
 
     def "getTokens"() {
         when:
-        var tokens = scanner.getTokens("var a")
+        def tokens = scanner.getTokens("var a = 123")
+        def expectedTokens = [
+                new Scanner.Token(TokenType.VAR),
+                new Scanner.Token(TokenType.VAR_NAME, "a"),
+                new Scanner.Token(TokenType.EQUAL),
+                new Scanner.Token(TokenType.INT_NUMBER, 123),
+                new Scanner.Token(TokenType.EOF),
+        ]
         then:
-        tokens.size() == 2
-        tokens[0] == new Scanner.Token(TokenType.VAR)
-        tokens[1] == new Scanner.Token(TokenType.VAR_NAME)
+        tokens == expectedTokens
     }
 }
